cb50168fe236b0a7cc684fc3f1813e0e4d2ebab7
hgalindo <helton.isac@gmail.com>
2018-03-25 16:17:09 -0300

    Circle CI Build

---
+++ misc/download_keystore.sh
@@ -0,0 +1,9 @@
+# use curl to download a keystore from $SIGNING_KEY_URI, if set,
+# to the path/filename set in $KEYSTORE.
+if [[ $KEYSTORE && ${KEYSTORE} && $SIGNING_KEY_URI && ${SIGNING_KEY_URI} ]]
+then
+    echo "Keystore detected - downloading..."
+    curl -L -o ${KEYSTORE} ${SIGNING_KEY_URI}
+else
+    echo "Keystore uri not set.  .APK artifact will not be signed."
+fi
