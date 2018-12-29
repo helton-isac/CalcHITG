d69cf55da8a7f4e605c144125a8cb916ae574451
hgalindo <helton.isac@gmail.com>
2018-12-29 12:02:22 -0200

    Changing Gradle from Groovy to Kotlin

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,24 @@
+object Versions {
+    const val compileSdk = 28
+    const val minSdk = 19
+    const val targetSdk = 28
+
+    // Android
+    const val kotlin = "1.3.11"
+    const val appCompat = "28.0.0"
+    const val constraintLayout = "1.1.3"
+
+    // Libraries
+    const val firebaseCore = "16.0.6"
+    const val crashlytics = "2.9.5@aar"
+    const val fabricToolsGradle = "1.25.4"
+    const val googleServices = "3.2.0"
+
+    // Tests
+    const val espressoCore = "3.0.2"
+    const val runner = "1.0.2"
+    const val junit = "4.12"
+
+    // Gradle
+    const val gradle = "3.2.1"
+}
